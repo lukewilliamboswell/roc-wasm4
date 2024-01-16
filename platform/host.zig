13123ee100186b821a47239ecdcc82538f2c089b
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-01-15 22:38:31 -0800

    clean up memory allocator

--- platform/host.zig
+++ platform/host.zig
@@ -12,6 +12,10 @@ const RocList = list.RocList;
 const utils = @import("vendored/utils.zig");
 
 const ALIGN = 2 * @alignOf(usize);
+const Range = std.bit_set.Range;
+comptime {
+    std.debug.assert(ALIGN >= @sizeOf(Range));
+}
 
 const TRACE_ALLOC = false;
 
@@ -30,10 +34,10 @@ var rnd = prng.random();
 export fn roc_alloc(requested_size: usize, alignment: u32) callconv(.C) *anyopaque {
     _ = alignment;
     // Leave extra space to store allocation size.
-    if (TRACE_ALLOC) {
-        w4.tracef("alloc -> requested size %d", requested_size);
-    }
     const size = requested_size + MEM_CHUNK_SIZE;
+    if (TRACE_ALLOC) {
+        w4.tracef("alloc -> requested size %d, full size %d, chunks %d", requested_size, size, size / MEM_CHUNK_SIZE);
+    }
 
     var chunk_size: usize = 0;
     var start_index: usize = 0;
@@ -50,20 +54,32 @@ export fn roc_alloc(requested_size: usize, alignment: u32) callconv(.C) *anyopaq
         @panic("ran out of memory");
     }
 
-    const exclusive_end_index = current_index + 1;
+    const exclusive_end_index = current_index;
     const range = .{ .start = start_index, .end = exclusive_end_index };
     if (TRACE_ALLOC) {
         w4.tracef("alloc -> start %d, end %d", start_index, exclusive_end_index);
     }
     free_set.setRangeValue(range, false);
 
-    const size_addr = mem_base + start_index * MEM_CHUNK_SIZE;
-    const data_addr = size_addr + MEM_CHUNK_SIZE;
+    const base_addr = mem_base + start_index * MEM_CHUNK_SIZE;
+    const data_addr = base_addr + MEM_CHUNK_SIZE;
 
-    const size_ptr: *usize = @ptrFromInt(size_addr);
-    size_ptr.* = chunk_size;
+    const data_ptr: [*]usize = @ptrFromInt(data_addr);
 
-    return @ptrFromInt(data_addr);
+    // Zero all memory before passing to Roc.
+    var i: usize = 0;
+    while (i < (chunk_size - MEM_CHUNK_SIZE) / MEM_CHUNK_SIZE) : (i += 1) {
+        const usizes = MEM_CHUNK_SIZE / @sizeOf(usize);
+        comptime var j = 0;
+        inline while (j < usizes) : (j += 1) {
+            data_ptr[i * usizes + j] = 0;
+        }
+    }
+
+    const range_ptr: *Range = @ptrFromInt(base_addr);
+    range_ptr.* = range;
+
+    return @ptrCast(data_ptr);
 }
 
 export fn roc_realloc(old_ptr: *anyopaque, new_size: usize, old_size: usize, alignment: u32) callconv(.C) ?*anyopaque {
@@ -83,16 +99,13 @@ export fn roc_realloc(old_ptr: *anyopaque, new_size: usize, old_size: usize, ali
 export fn roc_dealloc(c_ptr: *anyopaque, alignment: u32) callconv(.C) void {
     _ = alignment;
     const data_addr = @intFromPtr(c_ptr);
-    const size_addr = data_addr - MEM_CHUNK_SIZE;
-    const size_ptr: *usize = @ptrFromInt(size_addr);
-    const size = size_ptr.*;
+    const base_addr = data_addr - MEM_CHUNK_SIZE;
+    const range_ptr: *Range = @ptrFromInt(base_addr);
+    const range = range_ptr.*;
 
-    const start_index = (size_addr - mem_base) / MEM_CHUNK_SIZE;
-    const exclusive_end_index = start_index + size / MEM_CHUNK_SIZE + 1;
     if (TRACE_ALLOC) {
-        w4.tracef("free -> start %d, end %d", start_index, exclusive_end_index);
+        w4.tracef("free -> start %d, end %d", range.start, range.end);
     }
-    const range = .{ .start = start_index, .end = exclusive_end_index };
     free_set.setRangeValue(range, true);
 }
 
